250d008194b7695bc746bb81fb15ec52a5965099
Daniel Sarpong <dansarpong.me@gmail.com>
2023-04-30 22:34:27 +0000

    Modified and eliminated errors in read.py

--- read.py
+++ read.py
@@ -9,7 +9,7 @@ miso = Pin(19, Pin.OUT)
 spi = SPI(baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
 sda = Pin(5, Pin.OUT)
 
-# Function to read RFID UID (return UID)
+# Function to read RFID UID with the scanner (return UID)
 def read_rfid():
     uid = ""
     while uid == "":
@@ -21,49 +21,45 @@ def read_rfid():
                 uid = ("0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
                 sleep_ms(100)
     return uid
-    
-    
+
 # Function to write new user to database
 def sdwrite():
-    print("Write")
+    print("Write to database")
     UID = read_rfid()
     if not isStored(UID):
         index = input("Enter index number : ")
         with open("/file.csv", 'a') as f:
                 f.write("{}, {}\n".format(UID,index))
-                print("worked successfully")
+                print("{} added to database!".format(index))
     else:
-        print("User in database")
+        print("User already in database!")
            
 # Function to check if user data is stored in database
-def isStored(uid):
-    try:
-        uid, index = go_through(uid)
-        return uid == uid
-    except:
-        return False
+def isStored(given_uid):
+    uid, index = go_through(given_uid)
+    return given_uid == uid
 
 #Function to go through database and return id and index
 def go_through(uid):
     with open("/file.csv", 'r') as f:
         while True:
             userInfo = f.readline()
-            userUID, userIndex = userInfo.split(", ")
-            userIndex = userIndex.strip()
-            if not userInfo:
-                break
+            if userInfo:
+                userUID, userIndex = userInfo.split(", ")
+                userIndex = userIndex.strip()
+            else:
+                return "", ""
+            
             if userUID == uid:
-                return userUID, userindex
+                return userUID, userIndex
 
 # Function to validate user by checking through database
 def check_user():
     userId = read_rfid()
-    try:
-        uid, index = go_through(userId)
-    except:
-        index = "Not in list"
-    return index
+    uid, index = go_through(userId)
+    return index if index != "" else "User not found!"
 
+# test
 for i in range(3):
     sdwrite()
     
